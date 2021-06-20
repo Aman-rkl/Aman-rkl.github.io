5c6e3b431d4b6323e6950ef1caac9c90fa3f4de2
Aman Sagar <120CE0604@nitrkl.ac.in>
2021-06-20 16:49:47 +0530

    Create dyno.js

---
+++ dyno.js
@@ -0,0 +1,24 @@
+alert("there is an alert");
+var x=100;
+document.write(x);
+var text = "hello,world!";
+document.write(text);
+var user = prompt("please enter your name ");
+alert(user);
+var result = confirm("Do you really want to leave this page?");
+if (result == true) {
+  alert("Thanks for visiting");
+}
+else {
+  alert("Thanks for staying with us");
+}
+var course = {name: "JS", lessons: 41};
+document.write(course.name.length);
+var y = 7;
+document.write(Math.random(y));
+function myAlert() {
+   alert("Hi");
+}
+setInterval(myAlert, 3000);
+var c= 90;
+cosole.log(c);
